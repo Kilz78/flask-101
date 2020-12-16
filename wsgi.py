baccb71711e19bdd4ebd9c8a2595db808622d5b2
Guillaume Le Marec <lemarec.sentr@gmail.com>
2020-12-16 12:50:06 +0100

    add get products route

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,18 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Helfefelo World!"
+@app.route('/api/v1/products')
+def products():
+    #test2=[1,2,3]
+    test = {
+            1: { 'id': 1, 'name': 'Skello' },
+            2: { 'id': 2, 'name': 'Socialive.tv' },
+           }
+    return jsonify(list(test.values()))
+
